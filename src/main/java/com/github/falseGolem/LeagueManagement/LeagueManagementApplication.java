7b88ba9732b7ead26609057425b6b6268a047e90
Krzysztof Nowak <krzysztof.nowak@jamfsoftware.com>
2017-09-12 21:00:49 +0200

    First thing first…

---
+++ src/main/java/com/github/falseGolem/LeagueManagement/LeagueManagementApplication.java
@@ -0,0 +1,12 @@
+package com.github.falseGolem.LeagueManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueManagementApplication.class, args);
+	}
+}
